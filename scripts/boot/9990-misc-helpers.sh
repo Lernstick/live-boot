ea55d8d7cdb1454975ee2b057eb3de1dbd891105
Gaudenz Steinlin <gaudenz@debian.org>
2014-10-08 15:53:45 +0200

    Human readable device label for LUKS prompt
    Show the vendor and model strings as well as the partition number in
    addition to the raw device node.

--- scripts/boot/9990-misc-helpers.sh
+++ scripts/boot/9990-misc-helpers.sh
@@ -842,14 +842,20 @@ open_luks_device ()
 	# check for plymouth
 	[ -x /bin/plymouth ] && plymouth --ping && plymouth="y"
 
-	if [ "${plymouth}" == "y" ]
+    # export udisk properties (used to get a nice device label)
+    unset ID_VENDOR_ENC ID_MODEL_ENC ID_PART_ENTRY_NUMBER
+    eval $(udevadm info --name=${dev} --query=property -x)
+    [ -z "${ID_VENDOR_ENC}" -a -z "${ID_MODEL_ENC}" ] && ID_MODEL_ENC="Unidentified device"
+    label="$(echo -e "${ID_VENDOR_ENC}${ID_MODEL_ENC}, Partition ${ID_PART_ENTRY_NUMBER} (${dev})")"
+
+	if [ "${plymouth}" = "y" ]
 	then
 		cryptkeyscript="plymouth ask-for-password --prompt"
 		# Plymouth will add a : if it is a non-graphical prompt
-		cryptkeyprompt="Please unlock disk ${dev}"
+		cryptkeyprompt="Please unlock disk '${label}'"
 	else
 		cryptkeyscript="/lib/cryptsetup/askpass"
-		cryptkeyprompt="Please unlock disk ${dev}: "
+		cryptkeyprompt="Please unlock disk '${label}': "
 	fi
 
 	while true
@@ -865,11 +871,13 @@ open_luks_device ()
 		fi
 
 		echo >&6
-		retryprompt="There was an error decrypting ${dev} ... Retry? [Y/n]"
-		if [ "${plymouth}" == "y" ]
+		retryprompt="There was an error decrypting '${label}' ... Retry? [Y/n]"
+		if [ "${plymouth}" = "y" ]
 		then
 			plymouth display-message --text "${retryprompt}"
+			plymouth pause-progress
 			answer=$(plymouth watch-keystroke --keys="YNyn")
+			plymouth unpause-progress
 		else
 			echo -n "${retryprompt} " >&6
 			read answer
@@ -877,7 +885,7 @@ open_luks_device ()
 
 		if [ "$(echo "${answer}" | cut -b1 | tr A-Z a-z)" = "n" ]
 		then
-			[ "${plymouth}" == "y" ] && plymouth display-message --text ""
+			[ "${plymouth}" = "y" ] && plymouth display-message --text ""
 			return 2
 		fi
 	done
